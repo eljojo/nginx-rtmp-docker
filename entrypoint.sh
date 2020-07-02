d00467a4d5852e8dba9797b464d87edf8659d48e
José Albornoz <jojo@eljojo.net>
2020-07-02 16:58:03 -0400

    add support for more env vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-
+env
 echo "configuring nginx"
 /bin/sed -i 's/UPSTREAM_HOST/'"$UPSTREAM_HOST"'/g' /etc/nginx/nginx.conf
 /bin/sed -i 's/STREAM_NAME/'"$STREAM_NAME"'/g' /etc/nginx/nginx.conf
 
-echo $UPSTREAM_HOST
-echo $STREAM_NAME
+/bin/sed -i 's/STREAM_NAME/'"$STREAM_NAME"'/g' /var/www/static/index.html
+/bin/sed -i 's/LEGACY_DOMAIN/'"$LEGACY_DOMAIN"'/g' /var/www/static/index.html
+/bin/sed -i 's/DOMAIN/'"$DOMAIN"'/g' /var/www/static/index.html
 
 exec nginx -g "daemon off;"
